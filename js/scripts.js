866f0e668c27974b195a19e8c39c26d2583e5861
Francisco Ferraris <francisco_ff@hotmail.com>
2019-03-09 11:27:27 +0000

    Corrects birthday date format and starts with Prev Next buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,18 @@ const cardsGallery = document.querySelector("#gallery");
 const modalBody = document.querySelector("body");
 
 const appendGallery = (allEmployees) => {
-    allEmployees.forEach((employee) => {
-        let employeePicture = employee.picture.large;
-        let employeeName = `${employee.name.first} ${employee.name.last}`;
-        let employeeEmail = employee.email;
-        let employeeCity = `${employee.location.city}, ${employee.location.state}`;
-        let employeeFullAddress = `${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}`;
-        let employeeRawBirthday = employee.dob.date;
-        let employeeBirthday = employeeRawBirthday.replace(/-/g, '/').match(/^[^T]+/);
+    allEmployees.forEach((employee, index) => {
+        
         let employeeCard = document.createElement("div");
         employeeCard.classList.add("card");
         employeeCard.innerHTML = `
             <div class="card-img-container">
-                <img class="card-img" src="${employeePicture}" alt="profile picture">
+                <img class="card-img" src="${employee.picture.large}" alt="profile picture">
             </div>
             <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${employeeName}</h3>
-                <p class="card-text">${employeeEmail}</p>
-                <p class="card-text cap">${employeeCity}</p>
+                <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <p class="card-text">${employee.email}</p>
+                <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
             </div>
         `;
         cardsGallery.appendChild(employeeCard);
@@ -28,28 +22,33 @@ const appendGallery = (allEmployees) => {
         employeeCard.addEventListener ("click", () => {
             let employeeModal = document.createElement("div");
             employeeModal.classList.add("modal-container");
+            let employeeRawBirthday = employee.dob.date.match(/(\d+)-(\d+)-(\d+)/);
             employeeModal.innerHTML =
             `<div class="modal">
                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                 <div class="modal-info-container">
-                <img class="modal-img" src="${employeePicture}" alt="profile picture">
+                <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
                 <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
                 <p class="modal-text">${employee.email}</p>
-                <p class="modal-text cap">${employee.location.city}, ${employee.location.state}</p>
+                <p class="modal-text cap">${employee.location.city}</p>
                 <hr>
-                <p class="modal-text">${employee.cell}</p>
-                <p class="modal-text">${employeeFullAddress}</p>
-                <p class="modal-text">Birthday: ${employeeBirthday}</p>
+                <p class="modal-text">Tel: ${employee.cell}</p>
+                <p class="modal-text cap">${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                <p class="modal-text">Birthday: ${employeeRawBirthday[3]}/${employeeRawBirthday[2]}/${employeeRawBirthday[1]}</p>
+            </div>
+            <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
             </div>`
             modalBody.appendChild(employeeModal);
-            document.querySelector(".modal-close-btn").addEventListener('click', () => {
+            document.querySelector(".modal-close-btn").addEventListener("click", () => {
                 modalBody.removeChild(employeeModal);
-            })
+            });
+/*             document.querySelector("#modal-prev").addEventListener(); */
         });
     });
     
 }
-
 
 fetch(`https://randomuser.me/api/?results=${results}`)
 .then(response => response.json())
