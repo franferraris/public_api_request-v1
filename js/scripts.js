1f422321b1b5dcb84b8f553a22b26ad7c2ffe3e0
Francisco Ferraris <francisco_ff@hotmail.com>
2019-03-10 17:31:10 +0000

    Same functionality but with Javascript classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,119 @@
-const modalBody = document.querySelector("body");
-const cardsGallery = document.querySelector("#gallery");
 const totalResults = 12;
-let allModals = [];
-
+const engSpeakingCountries = 'au,ca,ie,nz,us';
 
 const appendGallery = (allEmployees) => {
     
-    allEmployees.forEach((employee, index) => {
-        
-        let employeeCard = document.createElement("div");
-        employeeCard.classList.add("card");
-        employeeCard.innerHTML = `
-            <div class="card-img-container">
-                <img class="card-img" src="${employee.picture.large}" alt="profile picture">
-            </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
-                <p class="card-text">${employee.email}</p>
-                <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
-            </div>
-        `;
-        cardsGallery.appendChild(employeeCard);
-
-        let employeeModal = document.createElement("div");
-        employeeModal.classList.add("modal-container");
-        
-        let employeeRawBirthday = employee.dob.date.match(/(\d+)-(\d+)-(\d+)/);
-        employeeModal.innerHTML =
-        `<div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-            <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
-            <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-            <p class="modal-text">${employee.email}</p>
-            <p class="modal-text cap">${employee.location.city}</p>
-            <hr>
-            <p class="modal-text">Tel: ${employee.cell}</p>
-            <p class="modal-text cap">${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-            <p class="modal-text">Birthday: ${employeeRawBirthday[3]}/${employeeRawBirthday[2]}/${employeeRawBirthday[1]}</p>
-        </div>
-        <div class="modal-btn-container">
-        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-        <button type="button" id="modal-next" class="modal-next btn">Next</button>
-        </div>`;
-        allModals.push(employeeModal);
-        
-        employeeCard.addEventListener ("click", () => {
-            modalBody.appendChild(employeeModal);
-        });
-        
-        employeeModal.querySelector(".modal-close-btn").addEventListener("click", () => {
-            modalBody.removeChild(employeeModal);
-        });
-        
-        let prevButton = employeeModal.querySelector('#modal-prev');
-        let nextButton = employeeModal.querySelector('#modal-next');
-
-        prevButton.addEventListener('click', () => {
-            modalBody.removeChild(employeeModal);
-            modalBody.appendChild(allModals[index - 1]);
-        })
-
-        nextButton.addEventListener('click', () => {
-            modalBody.removeChild(employeeModal);
-            modalBody.appendChild(allModals[index + 1]);
-        })
-
-        if (index === 0) {
-            prevButton.style.visibility = 'hidden';
-        } else if (index === allEmployees.length - 1) {
-            nextButton.style.visibility = 'hidden';
+    const modalBody = document.querySelector("body");
+    const cardsGallery = document.querySelector("#gallery");
+    let allModals = [];
+    
+    class employeeClass {
+        constructor (image, firstName, lastName, email, city, state, street, postcode, cell, rawBirthday, index) {
+            this.image = image;
+            this.name = `${firstName} ${lastName}`;
+            this.email = email;
+            this.location = `${city}, ${state}`;
+            this.fullAddress = `${street}, ${city}, ${state} ${postcode}`;
+            this.cell = cell;
+            this.rawBirthday = rawBirthday;
+            this.modal;
+            this.index = index;
         }
+    
+        get birthday () {
+            let birthday = this.rawBirthday.match(/(\d+)-(\d+)-(\d+)/);
+            return `${birthday[3]}/${birthday[2]}/${birthday[1]}`;
+        }
+    
+        createCard () {
+            let employeeCard = document.createElement("div")
+            employeeCard.classList.add("card")
+            employeeCard.innerHTML = `
+                <div class="card-img-container">
+                    <img class="card-img" src="${this.image}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${this.name}</h3>
+                    <p class="card-text">${this.email}</p>
+                    <p class="card-text cap">${this.location}</p>
+                </div>
+            `;
+            cardsGallery.appendChild(employeeCard);
+            
+            employeeCard.addEventListener ("click", () => {
+                modalBody.appendChild(this.modal);
+            });
+        }
+    
+        createModal () {
+            let employeeModal = document.createElement("div")
+            employeeModal.classList.add("modal-container")
         
+            employeeModal.innerHTML =
+            `<div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                <img class="modal-img" src="${this.image}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${this.name}</h3>
+                <p class="modal-text">${this.email}</p>
+                <p class="modal-text cap">${this.location}</p>
+                <hr>
+                <p class="modal-text">Tel: ${this.cell}</p>
+                <p class="modal-text cap">${this.fullAddress}</p>
+                <p class="modal-text">Birthday: ${this.birthday}</p>
+            </div>
+            <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>`;
+            allModals.push(employeeModal);
+            this.modal = employeeModal;
+            
+            employeeModal.querySelector(".modal-close-btn").addEventListener("click", () => {
+            modalBody.removeChild(this.modal)
+            });
+
+            let prevButton = employeeModal.querySelector('#modal-prev')
+            let nextButton = employeeModal.querySelector('#modal-next')
+    
+            prevButton.addEventListener('click', () => {
+                modalBody.removeChild(employeeModal)
+                modalBody.appendChild(allModals[this.index - 1])
+            });
+    
+            nextButton.addEventListener('click', () => {
+                modalBody.removeChild(employeeModal)
+                modalBody.appendChild(allModals[this.index + 1])
+            });
+    
+            if (this.index === 0) {
+                prevButton.style.visibility = 'hidden'
+            } else if (this.index === allEmployees.length - 1) {
+                nextButton.style.visibility = 'hidden'
+            };
+        }
+    }
+
+    allEmployees.forEach((employee, index) => {
+        let employeeProfile = new employeeClass (
+            employee.picture.large, 
+            employee.name.first,
+            employee.name.last, 
+            employee.email, 
+            employee.location.city, 
+            employee.location.state,
+            employee.location.street,
+            employee.location.postcode,
+            employee.cell,
+            employee.dob.date,
+            index
+            );
+        employeeProfile.createModal();
+        employeeProfile.createCard();
     });
 
 }
 
-fetch(`https://randomuser.me/api/?results=${totalResults}`)
+fetch(`https://randomuser.me/api/?results=${totalResults}&nat=${engSpeakingCountries}`)
 .then(response => response.json())
 .then(data => appendGallery(data.results))
