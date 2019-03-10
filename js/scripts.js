d5aaf5dc6c9e90e00af2f7af30a04e145fe18011
Francisco Ferraris <francisco_ff@hotmail.com>
2019-03-10 11:59:31 +0000

    Adds previous and next functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@ let allModals = [];
 
 
 const appendGallery = (allEmployees) => {
-    allEmployees.forEach((employee) => {
+    
+    allEmployees.forEach((employee, index) => {
         
         let employeeCard = document.createElement("div");
         employeeCard.classList.add("card");
@@ -21,35 +22,58 @@ const appendGallery = (allEmployees) => {
         `;
         cardsGallery.appendChild(employeeCard);
 
+        let employeeModal = document.createElement("div");
+        employeeModal.classList.add("modal-container");
+        
+        let employeeRawBirthday = employee.dob.date.match(/(\d+)-(\d+)-(\d+)/);
+        employeeModal.innerHTML =
+        `<div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+            <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+            <p class="modal-text">${employee.email}</p>
+            <p class="modal-text cap">${employee.location.city}</p>
+            <hr>
+            <p class="modal-text">Tel: ${employee.cell}</p>
+            <p class="modal-text cap">${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+            <p class="modal-text">Birthday: ${employeeRawBirthday[3]}/${employeeRawBirthday[2]}/${employeeRawBirthday[1]}</p>
+        </div>
+        <div class="modal-btn-container">
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>`;
+        allModals.push(employeeModal);
+        
         employeeCard.addEventListener ("click", () => {
-            let employeeModal = document.createElement("div");
-            employeeModal.classList.add("modal-container");
-            let employeeRawBirthday = employee.dob.date.match(/(\d+)-(\d+)-(\d+)/);
-            employeeModal.innerHTML =
-            `<div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-                <p class="modal-text">${employee.email}</p>
-                <p class="modal-text cap">${employee.location.city}</p>
-                <hr>
-                <p class="modal-text">Tel: ${employee.cell}</p>
-                <p class="modal-text cap">${employee.location.street}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-                <p class="modal-text">Birthday: ${employeeRawBirthday[3]}/${employeeRawBirthday[2]}/${employeeRawBirthday[1]}</p>
-            </div>
-            <div class="modal-btn-container">
-            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-            <button type="button" id="modal-next" class="modal-next btn">Next</button>
-            </div>`;
-            allModals.push(employeeModal);
             modalBody.appendChild(employeeModal);
-            document.querySelector(".modal-close-btn").addEventListener("click", () => {
-            modalBody.removeChild(employeeModal);
-            });
         });
+        
+        employeeModal.querySelector(".modal-close-btn").addEventListener("click", () => {
+            modalBody.removeChild(employeeModal);
+        });
+        
+        let prevButton = employeeModal.querySelector('#modal-prev');
+        let nextButton = employeeModal.querySelector('#modal-next');
+
+        prevButton.addEventListener('click', () => {
+            modalBody.removeChild(employeeModal);
+            modalBody.appendChild(allModals[index - 1]);
+        })
+
+        nextButton.addEventListener('click', () => {
+            modalBody.removeChild(employeeModal);
+            modalBody.appendChild(allModals[index + 1]);
+        })
+
+        if (index === 0) {
+            prevButton.style.visibility = 'hidden';
+        } else if (index === allEmployees.length - 1) {
+            nextButton.style.visibility = 'hidden';
+        }
+        
     });
-    
+
 }
 
 fetch(`https://randomuser.me/api/?results=${totalResults}`)
