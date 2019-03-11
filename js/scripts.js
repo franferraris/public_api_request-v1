0ce36988bd8591d85c811bc76f601010f54a2d6e
Francisco Ferraris <francisco_ff@hotmail.com>
2019-03-11 05:54:31 +0000

    Experimenting with pagination but won't continue for lack of time

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-const totalResults = 12;
+const totalResults = 24;
 const engSpeakingCountries = 'au,ca,ie,nz,us';
+let employeePages;
+let activePage = 1;
+let userInput;
 
 const appendGallery = (allEmployees) => {
     
@@ -73,23 +76,23 @@ const appendGallery = (allEmployees) => {
             modalBody.removeChild(this.modal)
             });
 
-            let prevButton = employeeModal.querySelector('#modal-prev')
-            let nextButton = employeeModal.querySelector('#modal-next')
+            let prevButton = employeeModal.querySelector("#modal-prev")
+            let nextButton = employeeModal.querySelector("#modal-next")
     
-            prevButton.addEventListener('click', () => {
+            prevButton.addEventListener("click", () => {
                 modalBody.removeChild(employeeModal)
                 modalBody.appendChild(allModals[this.index - 1])
             });
     
-            nextButton.addEventListener('click', () => {
+            nextButton.addEventListener("click", () => {
                 modalBody.removeChild(employeeModal)
                 modalBody.appendChild(allModals[this.index + 1])
             });
     
             if (this.index === 0) {
-                prevButton.style.visibility = 'hidden'
+                prevButton.style.visibility = "hidden"
             } else if (this.index === allEmployees.length - 1) {
-                nextButton.style.visibility = 'hidden'
+                nextButton.style.visibility = "hidden"
             };
         }
     }
@@ -111,6 +114,73 @@ const appendGallery = (allEmployees) => {
         employeeProfile.createModal();
         employeeProfile.createCard();
     });
+
+    let allEmployeeCards = document.querySelectorAll('.card');
+    let employeePages = Math.ceil(allEmployeeCards.length / 12);
+    
+    /***** Page list with links *****/
+    var pageList;
+    
+    const appendPageLinks = () => {
+        pageList = document.createElement('ol');
+        pageList.classList.add('pagination');
+        
+        // Creates amount of pages based on number of students
+        for (let i = 0; i < employeePages; i++) {
+        let pageButton = document.createElement('li');
+        let pageLink = document.createElement('a');
+        pageLink.innerHTML = i + 1;
+        pageLink.setAttribute('href', '#');
+        if (pageLink.innerHTML == activePage){
+        pageLink.classList.add('active');
+        }
+        pageButton.appendChild(pageLink);
+        pageList.appendChild(pageButton);
+        }
+        // Adds event listeners to the pages, using the number inside to set Active page
+        pageList.addEventListener ('click', (event) => {
+        if (event.target.tagName === 'A') {
+        pageList.querySelectorAll('a')
+        .forEach ( (e) => {
+            e.classList.remove('active');
+        });
+        event.target.classList.add('active');
+        activePage = event.target.innerHTML;
+        //  showPage();
+        }
+        }, false);
+        // Appends child to the div Page
+        modalBody.appendChild(pageList);
+    };
+
+    appendPageLinks();
+    
+    document.querySelector('.search-container').innerHTML = `
+    <form action="" method="get">
+        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>`;
+    const searchInput = document.querySelector("#search-input");
+
+    const employeePagination = () => {
+        // Checks the input
+        userInput = searchInput.value.toLowerCase();
+        
+        // Adds class visibile to students matching search
+        for (let i = 0; i < allEmployeeCards.length; i++) {
+            allEmployeeCards[i].style.display = "none";
+            if (allEmployeeCards[i].classList.contains("visible")) {
+            allEmployeeCards[i].classList.remove("visible");
+            }
+            if (allEmployeeCards[i].innerText.includes(userInput)) {
+            allEmployeeCards[i].classList.add("visible");
+            }
+        };
+
+    }
+
+    //employeePagination();
+
 
 }
 
